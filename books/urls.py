0237a5415d8b231c774716a2734abc0bd773d315
immanoj16 <mmaharana14@gmail.com>
2017-03-24 00:01:30 -0700

    Add Profile Model

--- books/urls.py
+++ books/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+]
